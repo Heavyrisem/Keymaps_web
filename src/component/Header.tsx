515990871011737d7b2aee40c6e5a8774dd620ea
heavyrisem <pyo1748@gmail.com>
2021-06-17 17:03:45 +0900

    init

---
+++ src/component/Header.tsx
@@ -0,0 +1,13 @@
+import react from 'react';
+import '../style/Header.css';
+
+
+function Header() {
+    return (
+        <header>
+
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/component/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Background from '../style/gradient.jpg';
+import '../style/Main.css';
+
+function Main() {
+	return (
+		<div className="Main">
+			123
+		</div>
+	);
+}
+
+export default Main;
